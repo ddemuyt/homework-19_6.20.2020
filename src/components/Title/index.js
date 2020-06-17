fdf38de5cb1f19dff435dfabebf98d9b4091adb3
Drake <drakedemuyt@gmail.com>
2020-06-16 22:54:07 -0400

    Moved in FriendsCard and some styling

---
+++ src/components/Title/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./style.css";
+
+function Title() {
+  return <div className="title"><p className="header">Employee Directory</p>
+    <p className="subheader">Click on carrots to filter by heading or use search box to narrow your results.</p></div>;
+}
+
+export default Title;
